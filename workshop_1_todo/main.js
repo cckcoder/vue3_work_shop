91e4b0070aef5d877b71cf306109d10bf0102f5b
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2022-12-11 22:37:26 +0700

    update: setup form and method

--- workshop_1_todo/main.js
+++ workshop_1_todo/main.js
@@ -1,2 +1,13 @@
 const app = Vue.createApp({
+  data() {
+    return {
+      todo: '',
+      todos: []
+    }
+  },
+  methods: {
+    submitTodo() {
+      this.todos.push(this.todo)
+    }
+  }
 }).mount('#app')
